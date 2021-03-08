e5aa363251eddea90cc9c071e3cdcd108187049b
i517819 <sean.yu@sap.com>
2021-03-08 15:15:20 +0800

    fix : 更新 152. 乘积最大子数组 题解

--- src/main/java/algorithms/dp/No132.java
+++ src/main/java/algorithms/dp/No132.java
@@ -8,7 +8,7 @@ package algorithms.dp;
  */
 public class No132 {
     public static void main(String[] args) {
-        System.out.println(new Solution132_2().minCut("aab"));
+        System.out.println(new Solution132II().minCut("leet"));
     }
 }
 
@@ -20,8 +20,9 @@ public class No132 {
  * 那么s[0]到s[k-1]的串，分割次数一定是最少
  * <p>
  * 2. 确定状态
- * dp(i)表示前i个字符可以分割成最少的回文串的个数，那么有
- * 如果存在k，使得s[k ~ i-1]是回文，那么dp(i) = min{ dp(k) } + 1 ， 0 <= k <= i-1
+ * dp(i)表示前i个字符可以分割成最少的回文串的个数，那么有：
+ * 如果s[0~i-1]是回文，那么dp(i) = 0
+ * 如果s[0~i-1]不是回文，如果存在k，使得s[k ~ i-1]是回文，那么dp(i) = min{ dp(k) } + 1 ， 1 <= k <= i-1
  */
 class Solution132 {
     public int minCut(String s) {
@@ -32,15 +33,21 @@ class Solution132 {
         int n = s.length();
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            dp[i] = i;
-            for (int k = 0; k <= i - 1; k++) {
-                //如果k到i-1是回文串
-                if (isPalindrome(s.substring(k, i))) {
-                    dp[i] = Math.min(dp[i], dp[k] + 1);
+            //长度为i的字符串，至多分为i-1个回文子串，即每个字母为一个回文
+            dp[i] = i - 1;
+            if (isPalindrome(s.substring(0, i))) {
+                dp[i] = 0;
+            } else {
+                //k从1开始，是因为已经判断过，并知道0-i不是回文串了
+                for (int k = 1; k <= i - 1; k++) {
+                    //如果k到i-1是回文串
+                    if (isPalindrome(s.substring(k, i))) {
+                        dp[i] = Math.min(dp[i], dp[k] + 1);
+                    }
                 }
             }
         }
-        return dp[n] - 1;
+        return dp[n];
     }
 
     private boolean isPalindrome(String s) {
@@ -57,58 +64,59 @@ class Solution132 {
 }
 
 /**
- * 优化版本： 牺牲空间换时间，提前计算出s[i][j]是否是回文串
+ * 优化版本： 牺牲空间换时间，提前计算出s[i][j]是否是回文串（利用中心扩散）
  */
-class Solution132_2 {
+class Solution132II {
     public int minCut(String s) {
-        if (s == null || s.length() <= 1) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
         int n = s.length();
-        int[] dp = new int[n + 1];
+        char[] arr = s.toCharArray();
         boolean[][] isPalindrome = new boolean[n][n];
-        init(s, isPalindrome);
-        for (int i = 1; i <= n; i++) {
-            dp[i] = i;
-            for (int k = 0; k <= i - 1; k++) {
-                //如果k到i-1是回文串
-                if (isPalindrome[k][i - 1]) {
-                    dp[i] = Math.min(dp[i], dp[k] + 1);
+        int[] dp = new int[n + 1];
+        init(isPalindrome, arr);
+
+        for (int len = 1; len <= n; len++) {
+            dp[len] = len - 1;
+            if (isPalindrome[0][len - 1]) {
+                dp[len] = 0;
+            } else {
+                for (int k = 1; k < len; k++) {
+                    if (isPalindrome[k][len - 1]) {
+                        dp[len] = Math.min(dp[len], dp[k] + 1);
+                    }
                 }
             }
         }
-        return dp[n] - 1;
+        return dp[n];
     }
 
-    private void init(String s, boolean[][] isPalindrome) {
-        int n = s.length();
-        //找到所有奇数长度的回文串, 遍历每个字符，以每个字符为中心向两边扩散
-        for (int i = 0; i < n; i++) {
-            //长度为1的串必定是回文串
-            isPalindrome[i][i] = true;
-            int l = i - 1, r = i + 1;
-            while (l >= 0 && r < n) {
-                if (s.charAt(l) == s.charAt(r)) {
-                    isPalindrome[l][r] = true;
-                    l--;
-                    r++;
-                } else {
+    //初始化，预先找出所有的回文子串
+    private void init(boolean[][] isPalindrome, char[] arr) {
+        int l, r;
+        for (int i = 0; i < arr.length; i++) {
+            //找到以i为中心，所有奇数长度的回文
+            l = i;
+            r = i;
+            while (l >= 0 && r < arr.length) {
+                if (arr[l] != arr[r]) {
                     break;
                 }
+                isPalindrome[l][r] = true;
+                l--;
+                r++;
             }
-        }
-
-        //找到所有偶数长度的回文串，遍历前n-1个字符，以该字符为对称轴左侧中心，向两边扩散
-        for (int i = 0; i < n - 1; i++) {
-            int l = i, r = i + 1;
-            while (l >= 0 && r < n) {
-                if (s.charAt(l) == s.charAt(r)) {
-                    isPalindrome[l][r] = true;
-                    l--;
-                    r++;
-                } else {
+            //找到所有以i和i+1的中线为中心，所有偶数长度的回文
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < arr.length) {
+                if (arr[l] != arr[r]) {
                     break;
                 }
+                isPalindrome[l][r] = true;
+                l--;
+                r++;
             }
         }
     }
