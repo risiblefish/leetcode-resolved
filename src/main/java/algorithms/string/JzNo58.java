86e289a5659710fbf8a71a273715750557736758
i517819 <sean.yu@sap.com>
2021-06-23 14:38:05 +0800

    feat : 增加 [字符串] No. 459. 重复的子字符串 题解

---
+++ src/main/java/algorithms/string/JzNo58.java
@@ -0,0 +1,36 @@
+package algorithms.string;
+
+import org.omg.CORBA.OBJ_ADAPTER;
+
+/**
+ * 剑指 Offer 58 - II. 左旋转字符串
+ * @author Sean Yu
+ */
+public class JzNo58 {
+    public static void main(String[] args) {
+        String b = "a";
+        b += "c";
+        System.out.println(b);
+
+        System.out.println(appendStr(b,"c"));
+        System.out.println("b".equals("c"));
+    }
+
+    private static String appendStr(String s,String append){
+        s += append;
+        return s;
+    }
+}
+
+
+
+/**
+ * 思路：
+ * 如果允许使用库函数的话，可以用字符串切片
+ */
+class SolutionJzNo58 {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder sb = new StringBuilder(s.substring(n) + s.substring(0,n));
+        return sb.toString();
+    }
+}
---
+++ src/main/java/algorithms/string/No28.java
@@ -0,0 +1,62 @@
+package algorithms.string;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @author Sean Yu
+ */
+public class No28 {
+    public static void main(String[] args) {
+//        System.out.println(new Solution28().strStr("ababcaababcaabc", "ababcaabc"));
+//        System.out.println(new Solution28().strStr("mississippi", "issip"));
+        System.out.println(new Solution28().strStr("aabaabaaf", "aabaaf"));
+//        System.out.println(new Solution28().strStr("hello", "ll"));
+//        System.out.println(new Solution28().strStr("ababcaababcaabc", "ababcaabc"));
+        LinkedBlockingQueue lbq = new LinkedBlockingQueue();
+        LinkedBlockingDeque deque = new LinkedBlockingDeque();
+    }
+}
+
+class Solution28 {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0){
+            return 0;
+        }
+        //初始化部分匹配值表
+        int[] pmt = getTable(needle);
+        int len = haystack.length();
+        int j = 0;
+        for (int i = 0; i < len; i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                j = pmt[j - 1];
+            }
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int[] getTable(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int[] pmt = new int[n];
+        pmt[0] = 0;
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            while (j > 0 && arr[i] != arr[j]) {
+                j = pmt[j - 1];
+            }
+            if (arr[i] == arr[j]) {
+                j++;
+            }
+            pmt[i] = j;
+        }
+        return pmt;
+    }
+
+}
