ad96e57587145bce3f913d681959ce83faab1e97
i517819 <sean.yu@sap.com>
2022-02-06 09:27:02 +0800

    feat : 增加[数组] No.1748. 唯一元素的和 题解

---
+++ src/main/java/algorithms/array/No1748.java
@@ -0,0 +1,34 @@
+package algorithms.array;
+
+import java.util.Arrays;
+
+/**
+ * 1748. 唯一元素的和
+ * @author Sean Yu
+ */
+public class No1748 {
+}
+
+class Solution1748 {
+    public int sumOfUnique(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            boolean flag = true;
+            if(i - 1 >= 0){
+                if(nums[i] == nums[i-1]){
+                    flag = false;
+                }
+            }
+            if(i + 1 < nums.length){
+                if(nums[i] == nums[i+1]){
+                    flag = false;
+                }
+            }
+            if(flag){
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+}
