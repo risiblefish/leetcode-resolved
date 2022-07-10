48b5be12441632a5b84017c098694c83e01b7694
i517819 <sean.yu@sap.com>
2022-07-10 13:29:44 +0800

    fix : 修改[双指针] No.11. 盛水最多的容器 题解

--- src/main/java/algorithms/dualpointer/No11.java
+++ src/main/java/algorithms/dualpointer/No11.java
@@ -8,43 +8,35 @@ public class No11 {
 }
 
 /**
- * 暴力法（超时）
+ * 双指针
+ * 盛水面积 = 短板高度*宽度
+ * 一开始把左右指针分别放于左右两端，这样做是为了让宽度达到最大
+ * 假设左右2端各有一块板，且长短不一，
+ * （1）如果向内移动长板，则面积只会变小，因为短板长度只可能变得更小，宽度-1， 没有增大的可能性。
+ * （2）反之，如果向内移动短板，则原来的长板有可能变成短板，宽度-1， 新短板高度*（-1后的宽度） 有可能增大
+ * 所以，
+ * 要从短板方向上进行移动，
+ * 如果短板移动后，高度比之前的短板更小，则可跳过计算，因为宽度和高度都变小了
  */
-class SolutionI {
+class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
-        int currHeighest;
-        int max = 0;
-        for(int i = 0 ; i < n ; i++){
-            currHeighest = 0;
-            for(int j = n-1 ; j > i ; j--){
-                if(height[j] > currHeighest) {
-                    int square = Math.min(height[i],height[j]) * (j-i);
-                    max = Math.max(max,square);
-                    currHeighest = height[j];
-                }
+        int l = 0, r = height.length - 1;
+        int max = Math.min(height[l], height[r]) * (r - l);
+        while(l < r){
+            //是否要跳过本次计算
+            boolean pass = false;
+            if(height[l] <= height[r]){
+                l++;
+                pass = height[l] <= height[l-1];
+            }else{
+                r--;
+                pass = height[r] <= height[r+1];
+            }
+            if(!pass){
+                int sum = Math.min(height[l], height[r]) * (r - l);
+                max = Math.max(max, sum);
             }
         }
         return max;
-    }
-}
-
-/**
- * 双指针
- */
-class SolutionII {
-    public int maxArea(int[] height) {
-        int n = height.length;
-        int l = 0, r = n-1;
-        int square = 0;
-        while(l < r){
-            square = Math.max(square, Math.min(height[l],height[r]) * (r-l));
-            if(height[l] <= height[r]){
-                l++;
-            }else {
-                r--;
-            }
-        }
-        return square;
     }
 }
