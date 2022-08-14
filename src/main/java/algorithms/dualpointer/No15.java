5f6331646e8f42489a45fa198fefcbfcee3fe133
i517819 <sean.yu@sap.com>
2022-08-14 21:25:57 +0800

    feat : 增加[双指针] No.30. 三数之和 题解

--- src/main/java/algorithms/dualpointer/No15.java
+++ src/main/java/algorithms/dualpointer/No15.java
@@ -16,52 +16,44 @@ public class No15 {
 /**
  * 思路: 双指针
  * 难点是去重，去重需要做2件事：排序 + 相同点跳过
- *
+ * <p>
  * 时间复杂度：O(n^2)
  */
 class Solution15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
-        if (nums == null || nums.length < 3) {
-            return res;
-        }
         Arrays.sort(nums);
-        //排序后，从左到右遍历每个数，对于第i个数，从它的右边开始双指针夹逼，找到nums[l] + nums[r] = - nums[i]
-        for (int i = 0; i < nums.length; i++) {
-            //剪枝：如果nums[i]已经大于0，它右边的nums[l],nums[r]必然均大于nums[i]，所以和必然大于-nums[i]
-            if (nums[i] > 0) {
-                break;
-            }
+        List<List<Integer>> ans = new ArrayList();
+        for (int a = 0; a < nums.length; a++) {
             //去重
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if (a > 0 && nums[a] == nums[a - 1]) {
                 continue;
             }
-            int l = i + 1, r = nums.length - 1;
+            //剪枝：排序后，a <= b <= c, 又有 a + b + c = 0,  如果a已经>0， 那b+c必然也大于0，就可以停止遍历了
+            if (nums[a] > 0) {
+                break;
+            }
+            int l = a + 1, r = nums.length - 1;
+            int target = -nums[a];
             while (l < r) {
                 int sum = nums[l] + nums[r];
-                if (sum < -nums[i]) {
-                    l++;
-                } else if (sum > -nums[i]) {
-                    r--;
-                } else {
-                    List<Integer> list = new ArrayList();
-                    list.add(nums[i]);
-                    list.add(nums[l]);
-                    list.add(nums[r]);
-                    res.add(list);
-                    l++;
-                    r--;
+                if (sum == target) {
+                    //找到一个解之后，需要继续l++, r--， 比如 -5 0 1 2 3 4, 1+4是解, 2+3也是解
+                    ans.add(Arrays.asList(nums[a], nums[l++], nums[r--]));
                     //去重
-                    while (l < r && nums[l] == nums[l - 1]) {
+                    while (l < nums.length && nums[l] == nums[l - 1]) {
                         l++;
                     }
                     //去重
-                    while (l < r && nums[r] == nums[r + 1]) {
+                    while (r >= 0 && nums[r] == nums[r + 1]) {
                         r--;
                     }
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
